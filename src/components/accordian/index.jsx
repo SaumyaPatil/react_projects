4fda72295a73587e2e3a01bcd9fa299ceec96967
SaumyaPatil <saupatil10dec@gmail.com>
2025-03-26 15:59:49 +0530

    Adding styles

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import data from "./data";
+import "./styles.css";
 
 export default function Accordian(){
     const [selected, setSelected] = useState(null);
@@ -13,27 +14,29 @@ export default function Accordian(){
     
     return(
         <div className="wrapper">
-            {
-                
-                data && data.length > 0 ?
-                data.map(dataItem => 
-                    <div className="item">
-                        <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
-                        </div>
-                        {
-                            selected === dataItem.id ? 
-                            (
-                                <div className="content">{dataItem.answer}</div> 
-                            ) :
-                            null
-                        }  
-                    </div>  
-                )
+            <div className="accordian">
+                {
+                    
+                    data && data.length > 0 ?
+                    data.map(dataItem => 
+                        <div className="item">
+                            <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ? 
+                                (
+                                    <div className="content">{dataItem.answer}</div> 
+                                ) :
+                                null
+                            }  
+                        </div>  
+                    )
 
-                : <div>No data found!</div>
-            }
+                    : <div>No data found!</div>
+                }
+            </div>
         </div>
     )
 }
