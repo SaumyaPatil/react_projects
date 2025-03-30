1ae86aeba07784fd42b12484cbe1cdc8081f4adc
SaumyaPatil <saupatil10dec@gmail.com>
2025-03-30 19:18:00 +0530

    Star rating problem

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -13,11 +13,14 @@ export default function StarRating({noOfStars}){
         setRating(getCurrentIndex);
     }
 
+    // If you have hovered the mouse and without click you are leaving no star will be selected, then the state will be as per rating on leave to preserve the 
+    // clicked state.
     function handleMouseHover(getCurrentIndex){
         setHover(getCurrentIndex);
     }
 
     function handleMouseLeave(){
+        //This is because if I select say 4 stars and I leave then it should stay selected. And on hovering, it should retain the previous state.
         setHover(rating);
     }
 
@@ -29,15 +32,18 @@ export default function StarRating({noOfStars}){
             //Destructuring of array 
             [...Array(noOfStars)].map((_, index)=>{
                 // console.log(_);
-                index += 1;   //Why this?
+                index += 1;   //This is to pass as 1, 2, 3, 4, 5 this way
                 return <FaStar
                     key={index}
+
+                    // This will render the stars when active or inactive as hover or rating changes, if the stars are active then it will show yellow and if not then it will show black 
+                    // for all stars.
                     className={index <= (hover || rating) ? 'active' : 'inactive'}
 
                     onClick={()=>handleClick(index)}
                     onMouseMove={()=>handleMouseHover(index)}
                     onMouseLeave={()=>handleMouseLeave()}
-                    size={40} //Whats this?
+                    size={40} //Whats this for?
                 />
             })
         }
