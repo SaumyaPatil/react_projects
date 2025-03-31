e5db1ff91e42995f87c96937c030a697f946f3b9
SaumyaPatil <saupatil10dec@gmail.com>
2025-03-31 21:58:27 +0530

    Project 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Accordian from './components/accordian'
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -15,10 +16,13 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* Star rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
 
       {/* Image slider component */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'}/> */}
+
+      {/* Load more data */}
+      <LoadMoreData/>
     </div>
   )
 }
